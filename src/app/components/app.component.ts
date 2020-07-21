9b58844eac49f8719938b5c9ae9fbaca5be91b91
MaksKrasn <uw5ekr@gmail.com>
2020-07-21 20:02:55 +0300

    Event remove is OK

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -9,10 +9,19 @@ import { Product } from '../models/product';
 export class AppComponent {
   title = 'product-list';
 
-  product: Product= new Product(1,'Pdoduct1','Bike Accsosuares',1000,'Bike lights');
+  products: Product[] = [];
+
+  constructor(){
+    this.products.push(new Product(1, 'Product1', 'Category1', 1000, 'Description1'));
+    this.products.push(new Product(2, 'Product2', 'Category2', 500, 'Description2'));
+    this.products.push(new Product(3, 'Product3', 'Category3', 3000, 'Description3'));
+    this.products.push(new Product(4, 'Product4', 'Category4', 200, 'Description4'));
+  }
 
   removeProduct(id: number) {
     console.log('Remove: ' + id);
+    let idx  = this.products.findIndex(prod => prod.id == id);
+    this.products.splice(idx, 1);
   }
 
   editProduct(id: number) {
