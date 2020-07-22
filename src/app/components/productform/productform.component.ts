7be8365f0ab5fa4fcd1f114d684cf97c098d7459
Maks Krasnikov <uw5ekr@gmail.com>
2020-07-22 10:57:05 +0300

    Modified form

--- src/app/components/productform/productform.component.ts
+++ src/app/components/productform/productform.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import{ FormsModule } from '@angular/forms'
+//import{ FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'productform',
@@ -9,10 +9,19 @@ import{ FormsModule } from '@angular/forms'
 })
 export class ProductformComponent implements OnInit {
 
-  @Input() @Output() product: Product;
+  @Input() @Output() product: Product = null;
+  @Output() saveEvent: EventEmitter<Product> = new EventEmitter<Product>();
 
-  constructor(product: Product) { 
+  constructor(product?: Product) { 
+    if(product != null){
+      this.product = product;
+    }
+  }
+
+  onClickSave(product: Product){
+    console.log(product);
     this.product = product;
+    this.saveEvent.emit(product);
   }
 
   ngOnInit(): void { 
