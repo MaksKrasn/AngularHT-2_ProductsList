8c515a62548bed75567dc34694c817f99e97e415
Maks Krasnikov <uw5ekr@gmail.com>
2020-07-23 16:36:42 +0300

    save midified before close IDE

--- src/app/components/editprodform/editprodform.component.ts
+++ src/app/components/editprodform/editprodform.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
-  selector: 'app-editprodform',
+  selector: 'editprodform',
   templateUrl: './editprodform.component.html',
   styleUrls: ['./editprodform.component.css']
 })
 export class EditprodformComponent implements OnInit {
+  product: Product = null;
+  @Input() name: string = '';
+  @Input() category: string = '';
+  @Input() price: number;
+  @Input() description: string = '';
+  @Output() editEvent: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() cancelEvent: EventEmitter<null> = new EventEmitter<null>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(product?: Product) { 
+    if(product != null){
+      this.product = product;
+    }
   }
 
+  onClickEdit(){
+    this.product = new Product(0, this.name, this.category, this.price , this.description);
+    console.log(this.product);
+    this.editEvent.emit(this.product);
+  }
+
+  onClickCancel(){
+    console.log('Cancel');
+    this.cancelEvent.emit();
+  }
+
+  ngOnInit(): void { 
+  }
 }
